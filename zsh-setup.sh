be8d4300e503f7117dc23e5c5564166d608ebb34
Amir Nazari <amir@nazari.sydney>
2022-06-27 20:55:26 +1000

    Added a function to check if a command is installed or not

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-# Check to see if ZSH is already installed.
+installCheck() {
+    command -v "$1" >/dev/null || {
+        echo "Please install $1 before proceeding!"
+        exit 1
+    }
+}
+
+# Check if ZSH and Git is installed
+installCheck "zsh"
+installCheck "git"
+
+# Check to see if OMZ is already installed.
 if ! [ -d "$HOME/.oh-my-zsh" ]; then
     echo "Oh-my-zsh is not installed, installing now."
     git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh 2>/dev/null && {
@@ -21,7 +32,7 @@ if ! [ -d "$HOME/.oh-my-zsh" ]; then
             read -r "Would you like the default shell to be ZSH? " yn
             case $yn in
             [Yy]*)
-                chsh -s "$(which zsh)"
+                chsh -s "$(command -v zsh)"
                 break
                 ;;
             [Nn]*) break ;;
@@ -33,11 +44,12 @@ fi
 
 echo "Oh-my-zsh installed, proceeding with plugin installation."
 
-# Check to see if the plugins are already installed.
+# Find zshrc file and plugin line.
 pluginLine="$(grep -E "^[^#].+(\)$)" "$HOME"/.zshrc)" || {
     echo "Failed to find .zshrc file or plugin line."
     exit 1
 }
+
 # Check to see if the plugins are already installed.
 if echo "$pluginLine" | grep -q "zsh-autosuggestions"; then
     echo "zsh-autosuggestions already installed!"
@@ -51,16 +63,14 @@ fi
 
 # Download syntax highlighting
 echo "Downloading syntax highlighting now!"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME"/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME"/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting 2>/dev/null
 
 # Download autosuggestions
 echo "Downloading autosuggestions now!"
-git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME"/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME"/.oh-my-zsh/custom/plugins/zsh-autosuggestions 2>/dev/null
 
 tempFile=$(mktemp)
 sed -E "s?^plugins=\((.*)\)?\plugins=(\1 zsh-autosuggestions zsh-syntax-highlighting)?" "$HOME"/.zshrc >"$tempFile"
 cat "$tempFile" >"$HOME"/.zshrc
 rm "$tempFile"
 echo "Oh-my-zsh and plugins successfully installed. Please close and reopen your shell to observe the changes."
-
-
